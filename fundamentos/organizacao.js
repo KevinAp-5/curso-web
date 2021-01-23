d78291abfd2fec1f1d44d609aa09f33ac8457cc6
KevinAp-5 <keven.clash3@gmail.com>
2021-01-23 07:56:13 -0300

    padrão de style e sintaxe do curso

---
+++ fundamentos/organizacao.js
@@ -0,0 +1,11 @@
+console.log(
+    "Hello, macacos!"
+)
+
+{
+    {
+        console.log("Olá");
+        console.log('Mundo!')  // padrão do curso
+        // Aula focada para organização do padrão do curso
+    }
+}
