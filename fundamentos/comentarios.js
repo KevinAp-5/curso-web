a564a9d7370b58b64387c2a11991d5eb549ffe59
KevinAp-5 <keven.clash3@gmail.com>
2021-01-23 14:43:28 -0300

    uso de comentários

---
+++ fundamentos/comentarios.js
@@ -0,0 +1,20 @@
+// comentário de uma linha
+console.log('Linha 1')  // comentário pode ser do lado também
+
+/*
+ comentário de
+ múltiplas linhas
+ */
+console.log('Linha 2')
+
+/*
+ * Comentário de
+ * múltiplas linhas
+ * com *
+ * aaaaa
+ *
+ */
+
+ // cuidado comentários idiotas U.U
+ // faça código legível e limpo ex: nomes claros, comentários, etc
+ // comente tudo que você acha importante comentar
