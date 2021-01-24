ef7e8e3bb57068a4639789e75d79415a67299cce
KevinAp-5 <keven.clash3@gmail.com>
2021-01-24 06:50:18 -0300

    basico de var, let e const

---
+++ fundamentos/variaveisEConstantes.js
@@ -0,0 +1,14 @@
+var a = 3  // 
+let b = 4  // existe diferença entre let e var. No momento, usaremos let
+
+var a = 30
+// let b = 40  vai dar erro
+b = 40
+
+console.log(a, b)
+a = 200
+console.log(a)
+
+const nome = 'kevin' // declarar uma constante
+// nome = 'joão'
+console.log(nome)
