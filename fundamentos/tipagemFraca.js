164681fe303d242b3b3b96214a00a604458d4386
KevinAp-5 <keven.clash3@gmail.com>
2021-01-24 07:10:42 -0300

    tipagem fraca

---
+++ fundamentos/tipagemFraca.js
@@ -0,0 +1,22 @@
+let meu_nome = 'kevin'
+console.log(meu_nome)
+console.log(typeof meu_nome)
+meu_nome = 12.21
+console.log(meu_nome)
+console.log(typeof meu_nome)
+
+// Evitar nomes genéricos e siglas
+let valor = '' // genérico
+let numero = 1 /* ao menos que o programa seja muito pequeno, usar nomes
+genérico pode complicar a leitura do código deixando-o mais confuso */
+
+let pqp = true // Produto químico perigoso... azideiakkk
+console.log(pqp)
+console.log(typeof pqp)
+
+/* outras pessoas podem ter diferentes interpretações da sigla
+por isso não é recomendado usar */
+
+// Escolhas de bons nomes é fundamental
+
+// prefira código claro á comentários de código tendo que explicar a sigla
