29e083d43940fbb29818c805bce5ee1473aad226
KevinAp-5 <keven.clash3@gmail.com>
2021-01-25 14:12:36 -0300

    objetos

---
+++ fundamentos/objeto.js
@@ -0,0 +1,21 @@
+const produto1 = {};
+
+produto1.nome = 'Tv super!';
+produto1.preco = 1200.99;
+produto1['desconto massa'] = 0.40 // evitar atributos com espaço
+
+console.log(produto1);
+
+const produto2 = { // não é a mesma coisa que um json
+    nome: 'celular top',
+    preco: 990.90,
+    a: {
+        x: false,
+        b: {
+            y = true
+        }
+    },
+}
+
+"{ 'nome': 'foda', 'preco': 20.2} " // isso é um json
+console.log(produto2)
