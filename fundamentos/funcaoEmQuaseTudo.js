32ee6e1ee0f625e98a4dc993c3c12313510fddec
KevinAp-5 <keven.clash3@gmail.com>
2021-01-25 15:31:09 -0300

    Quase tudo e funcao: importancia da funcao no js

---
+++ fundamentos/funcaoEmQuaseTudo.js
@@ -0,0 +1,11 @@
+console.log(typeof Object) // >>> function
+
+class Prod {} // >>> function. Qualquer classe é uma função
+console.log(typeof Prod)
+
+/*
+ * Entender como a funcão no javascript funciona é crucial para um js dev
+ * não entender a fundo a filosofia do javascript com funções é o motivo
+ * pelo qual muitas pessoas são amarguradas com a linguagem.
+ * "Se javascript fosse um país, função seria o presidente."
+*/
