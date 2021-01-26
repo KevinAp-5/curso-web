55b677c59eda289a8fdd6d56207bc292cfda7a12
KevinAp-5 <keven.clash3@gmail.com>
2021-01-26 12:51:56 -0300

    funcao x objeto

---
+++ fundamentos/24objeto2.js
@@ -0,0 +1,12 @@
+console.log(typeof Object)
+console.log(typeof new Object)
+
+const Cliente1 = function () {}
+
+console.log(typeof Cliente1)
+console.log(typeof new Cliente1)
+
+class Produto {} //padrão ES 2015 (JS6)
+// a classe é nada mais que uma sintexe sugar para instanciar funções
+console.log(typeof Produto)
+console.log(typeof new Produto)
