02e615ccab2058e5818407af73952c2c5fe4a76a
KevinAp-5 <keven.clash3@gmail.com>
2021-01-26 12:13:38 -0300

    Usando Var em loop 01

---
+++ fundamentos/19UsandoVarEmLoop1.js
@@ -0,0 +1,6 @@
+for (var x=0; x < 10; x++) { // o x só sai do laço quando x < 10 for false
+    // assim, quando ele sair vai ter o valor de 9 por ter sido aumentado
+    console.log(x)
+}
+// se fosse usado let, ja teria erros
+console.log('x =', x)
