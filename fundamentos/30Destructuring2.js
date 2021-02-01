aeb49fab2e43279ad9abdac23c8cedf32203e9de
KevinAp-5 <keven.clash3@gmail.com>
2021-02-01 20:36:08 -0300

    fixed typos

--- fundamentos/30Destructuring2.js
+++ fundamentos/30Destructuring2.js
@@ -1,6 +1,6 @@
-const [a] = [10] // criou a variavel a que recebeu o array [10]
+const [a] = [10] // criou a variavel 'a' que recebeu o number 10 do array
 // usamos colchetes porquê queremos pegar os elementos de um array
-console.log(a)
+console.log(typeof a)
 
 // podemos pular os valores, representando os ' ' entre as virgulas
 const [a1, , a3, , a5, a6 = 0] = [2, 10, 3, 20]
@@ -10,14 +10,14 @@ do array. a6 não é undefined porquê tem valor padrão */
 
 // arrays podem ter outros elementos do tipo array
 /* ignorando o primeiro elemento, dentro do array, ignorando o primeiro
-elemente e pegando o segundo elemento do array no indice 1 */
+elemento e pegando o segundo elemento do array no indice 1 */
 const [, [, nota]] = [[, 8, 8], [9, 6, 8]]
 /* o que está esta sintaxe está dizendo, ignore o primeiro elemento, vá para
 o segundo elemento(um array), ignore o primeiro elemento desse array, e pegue
-o segundo elemente*/
+o segundo elemento*/
 
 // Você não usaria esse código na pratica, porque é um código de difícil leitura
-console.log()
+console.log(nota)
 
 // tem uma leitura mais fácil se aplicar isso a objetos, mas em arrays não
 
