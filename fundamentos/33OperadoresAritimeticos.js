4d47f41ca23ebf8c2c7de90739e6a7a3d26c5a44
KevinAp-5 <keven.clash3@gmail.com>
2021-02-02 17:36:51 -0300

    operadores aritimeticos

---
+++ fundamentos/33OperadoresAritimeticos.js
@@ -0,0 +1,16 @@
+const [a, b, c, d] = [3, 10, 20, 16]; // usando destructuring
+
+const soma = a + b + c + d; // operação infix
+const subtracao = c - d;
+const multiplicacao = a * b;
+const divisao = a / b;
+const modulo = b % a;
+console.log(soma, subtracao, multiplicacao, divisao, modulo)
+
+// operador aritimético é um operador binário
+
+/*
+  d++ // postfix
+  ++d // prefix
+  a + b // infix
+*/
