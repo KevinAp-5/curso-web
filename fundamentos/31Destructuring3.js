e137250754a1f13ddeadb6ac924d2eaf138d4f83
KevinAp-5 <keven.clash3@gmail.com>
2021-02-01 21:16:10 -0300

    [FIX] problem with boisting and added comments

--- fundamentos/31Destructuring3.js
+++ fundamentos/31Destructuring3.js
@@ -1,8 +1,12 @@
-function random_number({min = 0, max = 1000}) { /* Essa operação já vai
+var random_number = function ({min = 0, max = 1000}) { /* função em var por
+    conta do hoisting */
+
+    /* Essa operação já vai
     receber um objeto e ja faremos a destructuring nele, assim não precisaremos
     usar a notação ponto, atribuir o valor a uma variavel, etc...
     Assim, já teremos a variavel min e max na função*/
     const valor = Math.random() * (max - min) + min
+    console.log('a')
     return Math.floor(valor)
 }
 
@@ -16,9 +20,8 @@ console.log(random_number(min_max)) // passando um objeto já definido
 console.log(random_number({min:40, max:600})) // passamos objeto como parametro
 console.log(random_number({min:400})) // já temos valor padrão, não vai dar erro
 console.log(random_number({})) // passamos um objeto vazio
-console.log(random_number()) // teremos um erro pois o objeto é undefined
+// console.log(random_number()) // teremos um erro pois o objeto é undefined
 // para esse erro podemos colocar um objeto como padrão na função
-// não está tendo erro por estar acontecendo um hosting com a função abaixo
 
 function random_number({min = 0, max = 1000} = {}) { /* Essa operação já vai
     receber um objeto e ja faremos a destructuring nele, assim não precisaremos
